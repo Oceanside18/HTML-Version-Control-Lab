e1e719f6469d6c85aac21ac653c12c8ab12dc8cb
Oceanside18 <Mya.Mccarthy@edu.sait.ca>
2024-11-16 17:15:24 -0700

    Update file.js

--- file.js
+++ file.js
@@ -2,8 +2,10 @@
 reference: https://www.uvm.edu/~bnelson/computer/javascript/pageheadersandfooters.html 
 Date (c Mya McCarthy, 2024)
 </footer>
-(if (< 12:00) { 
-    greeting = "good day";
-} else { 
-    
-})
+if (< 12) { 
+    greeting = "good morning!";
+} else if (< 17) { 
+     greeting = "Good afternoon!";
+} else {
+    greeting = "Good evening";
+}
