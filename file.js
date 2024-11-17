507b2fedfdc399b2426adde2d24b655541cd08be
Oceanside18 <Mya.Mccarthy@edu.sait.ca>
2024-11-16 17:08:52 -0700

    Update file.js

--- file.js
+++ file.js
@@ -2,3 +2,8 @@
 reference: https://www.uvm.edu/~bnelson/computer/javascript/pageheadersandfooters.html 
 Date (c Mya McCarthy, 2024)
 </footer>
+(if (< 12:00) { 
+    greeting = "good day";
+} else { 
+    
+})
